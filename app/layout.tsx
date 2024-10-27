6ceef68fe3194b9371578120810154aa2cfe0f5c
Yurii-7 <25harlam@gmail.com>
2024-10-27 01:23:58 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "@/app/ui/global.css";
+import { roboto } from "./ui/fonts";
+import React from "react";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${roboto.className} mantialiased`}>{children}</body>
     </html>
   );
 }
